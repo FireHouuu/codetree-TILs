33a589d1fd69ed545f391f4afe20b36bdce1cf8a
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-01-10 14:24:15 +0900

    Title: 최소공배수 구하기, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240110/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240110/최소공배수 구하기/find-the-least-common-multiple.py
@@ -2,7 +2,7 @@ n, m = tuple(map(int, input().split()))
 
 def gcx(n,m):
     gcx = 0
-    for i in range(max(n,m), n*m+1 ,1):
+    for i in range(max(n,m), n*m+1):
         if i%n==0 and i%m==0:
             gcx = i
             break
