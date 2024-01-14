cee392f79acdfb848ba3291cf409de73e5a14652
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-01-14 12:55:16 +0900

    Title: 2021년 날짜의 유무, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240114/2021년 날짜의 유무/with-or-without-2021.py
+++ 240114/2021년 날짜의 유무/with-or-without-2021.py
@@ -3,7 +3,9 @@ m, d = tuple(map(int, input().split()))
 def check(m, d):
     m_30 = [4, 6, 9, 11]
     m_31 = [1, 3, 5, 7, 8, 10, 12]
-    if m == 2:
+    if m > 12:
+        return False
+    elif m == 2:
         if d > 28:
             return False
     elif m in m_30:
