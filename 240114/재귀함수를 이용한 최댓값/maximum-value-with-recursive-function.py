3cfd48c1f27f85a2bc5e1d7aedb0a6bdd2667171
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-01-14 18:06:36 +0900

    Title: 재귀함수를 이용한 최댓값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240114/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 240114/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -2,7 +2,7 @@ n = int(input())
 _list = list(map(int, input().split()))
 
 def f(n):
-    if n == 1:
+    if n == 1 or n == 0:
         return max(_list[0], _list[1])
     return max(f(n-1), _list[n])
 
