90debc16114d55b0434341ca0cb4d9c669fd2341
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-01-15 16:24:03 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 172ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240115/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,25 @@
+n = int(input())
+_list = list(map(int, input().split()))
+
+
+def gcx(x):
+    global n
+    cnt = 0
+    tmp = []
+    if x <= 1:
+        out = 1
+        for k in range(n):
+            out *= _list[k]
+        return out
+    for i in range(n):
+        if _list[i] >= x and _list[i] % x == 0:
+            cnt += 1
+            tmp.append(i)
+    if cnt >= 2:
+        for j in tmp:
+            _list[j] /= x
+        return gcx(x) * x
+    else:
+        return gcx(x-1)
+
+print(int(gcx(10)))
