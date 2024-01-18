0b66fe8c713ecc656d819ddf01cc1d7f8e5a7c0e
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-01-18 13:15:39 +0900

    Title: Date to Date, Time: 118ms, Memory: 37MB, Status: Wrong Answer - CodeTree

---
+++ 240118/Date to Date/date-to-date.py
@@ -0,0 +1,10 @@
+m1, d1, m2, d2 = tuple(map(int, input().split()))
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+def day_cal(m, d):
+    m_d = 0
+    for i in range(1, m+1):
+        m_d += num_of_days[i]
+    return m_d + d
+
+print(day_cal(m2, d2) - day_cal(m1, d1))
