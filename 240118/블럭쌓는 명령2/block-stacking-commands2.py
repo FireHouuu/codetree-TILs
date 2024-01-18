359d4fbc35beaed9bc6ba7642e105aef9b208aac
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-01-18 14:30:58 +0900

    Title: 블럭쌓는 명령2, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 240118/블럭쌓는 명령2/block-stacking-commands2.py
+++ 240118/블럭쌓는 명령2/block-stacking-commands2.py
@@ -1,11 +1,11 @@
 n, k = tuple(map(int, input().split()))
-block = [0] * n
+block = [0] * (n+1)
 
 def do(a, b):
     for i in range(a, b+1):
         block[i] += 1
 
-for j in range(k):
+for _ in range(k):
     a, b = tuple(map(int, input().split()))
     do(a, b)
 
