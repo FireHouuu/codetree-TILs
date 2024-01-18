6d27c3728c77b7179093ee7a93e0ff3c3d10a5ac
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-01-18 13:23:03 +0900

    Title: Date to Date, Time: 149ms, Memory: 29MB, Status: Passed - CodeTree

--- 240118/Date to Date/date-to-date.py
+++ 240118/Date to Date/date-to-date.py
@@ -3,8 +3,8 @@ num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 def day_cal(m, d):
     m_d = 0
-    for i in range(1, m+1):
+    for i in range(1, m):
         m_d += num_of_days[i]
     return m_d + d
 
-print(day_cal(m2, d2) - day_cal(m1, d1))
+print(day_cal(m2, d2) - day_cal(m1, d1) + 1)
