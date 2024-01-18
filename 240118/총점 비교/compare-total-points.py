9b04033b2fce88087157a8927dc0c78cbc392bec
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-01-18 10:10:11 +0900

    Title: 총점 비교, Time: 224ms, Memory: 29MB, Status: Passed - CodeTree

--- 240118/총점 비교/compare-total-points.py
+++ 240118/총점 비교/compare-total-points.py
@@ -1,10 +1,20 @@
+from functools import cmp_to_key
+
 class student:
     def __init__(self, name, fist, second, third):
         self.name, self.fist, self.second, self.third = name, int(fist), int(second), int(third)
         
+def compare(x, y):
+    if x.fist + x.second + x.third > y.fist + y.second + y.third:
+        return 1
+    elif x.fist + x.second + x.third < y.fist + y.second + y.third:
+        return -1
+    else:
+        return 0
+
 n = int(input())
 _class = [student(*input().split()) for _ in range(n)]
-_class.sort(key=lambda x: x.fist + x.second + x.third)
+_class.sort(key=cmp_to_key(compare))
 
 for student in _class:
     print(student.name, student.fist, student.second, student.third)
