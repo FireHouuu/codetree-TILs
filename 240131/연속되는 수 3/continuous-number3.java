a364fa8fe45a0b185e88cc210928e7f3d5137527
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-01-31 17:20:37 +0900

    Title: 연속되는 수 3, Time: 207ms, Memory: 11MB, Status: Passed - CodeTree

--- 240131/연속되는 수 3/continuous-number3.java
+++ 240131/연속되는 수 3/continuous-number3.java
@@ -5,19 +5,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        int cnt = 1, max_cnt = 1;
+        int cnt = 0, max_cnt = 1;
         boolean was_plus = true;
         int cur = 0;
         for (int i = 1; i <= n; i++) {
             cur = sc.nextInt();
 
             if (cur > 0) {
-                if (was_plus == true) cnt++;
+                if (i == 0 || was_plus == true) cnt++;
                 else cnt = 1;
                 was_plus = true;
             }
             else {
-                if (was_plus == false) cnt++;
+                if (i == 0 || was_plus == false) cnt++;
                 else cnt = 1;
                 was_plus = false;
             }
