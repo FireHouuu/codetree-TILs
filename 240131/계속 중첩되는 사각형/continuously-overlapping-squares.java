d9740e809b9a72ad195fc1bc5ab2407c6003978b
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-01-31 16:30:51 +0900

    Title: 계속 중첩되는 사각형, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 240131/계속 중첩되는 사각형/continuously-overlapping-squares.java
+++ 240131/계속 중첩되는 사각형/continuously-overlapping-squares.java
@@ -5,10 +5,10 @@ public class Main {
     public static int[][] plane = new int[2*offset+1][2*offset+1];
     public static int blue_area, n;
 
-    public static void fill (int x1, int y1, int x2, int y2, boolean is_red) {
+    public static void fill (int x1, int y1, int x2, int y2, int is_red) {
         for (int i = x1; i < x2; i++) {
             for (int j = y1; j < y2; j++) {
-                if (is_red) plane[i][j] = 1;
+                if (is_red % 2 == 1) plane[i][j] = 1;
                 else plane[i][j] = 2;
             }
         }
@@ -16,9 +16,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        for (int i = 0; i < n/2; i++) {
-            fill(sc.nextInt()+offset, sc.nextInt()+offset, sc.nextInt()+offset, sc.nextInt()+offset, true);
-            fill(sc.nextInt()+offset, sc.nextInt()+offset, sc.nextInt()+offset, sc.nextInt()+offset, false);
+        int is_red = 1;
+        for (int i = 0; i < n; i++) {
+            fill(sc.nextInt()+offset, sc.nextInt()+offset, sc.nextInt()+offset, sc.nextInt()+offset, is_red);
+            is_red++;
         }
         blue_area = 0;
         for (int i = 0; i <= 2*offset; i++) 
