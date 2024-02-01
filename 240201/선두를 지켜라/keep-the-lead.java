c270157cec0e7b837b9aac5ad456a97e43c5ac5d
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-02-01 20:20:03 +0900

    Title: 선두를 지켜라, Time: 281ms, Memory: 29MB, Status: Passed - CodeTree

--- 240201/선두를 지켜라/keep-the-lead.java
+++ 240201/선두를 지켜라/keep-the-lead.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static int n, m;
-    public static int[] nrace = new int[1000^2];
-    public static int[] mrace = new int[1000^2];
+    public static int[] nrace = new int[1001 * 1000]; // 최대 n * t 시간까지 고려
+    public static int[] mrace = new int[1001 * 1000]; // 최대 m * t 시간까지 고려
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -14,7 +14,7 @@ public class Main {
         int ntime = 1, mtime = 1;
         for (int i = 0; i < n; i++) {
             nv = sc.nextInt(); nt = sc.nextInt();
-            while (nt-- > 0) {
+            for (int j = 0; j < nt; j++) {
                 nrace[ntime] = nrace[ntime-1] + nv;
                 ntime++;
             }
@@ -22,20 +22,21 @@ public class Main {
 
         for (int i = 0; i < m; i++) {
             mv = sc.nextInt(); mt = sc.nextInt();
-            while (mt-- > 0) {
+            for (int j = 0; j < mt; j++) {
                 mrace[mtime] = mrace[mtime-1] + mv;
                 mtime++;
             }
         }
+
         boolean past_n_first = true, cur_n_first = true;
-        for (int i = 1; i < 1000*1000; i++) {
+        int maxTime = Math.max(ntime, mtime); // 더 긴 시간을 최종 시간으로 설정
+        for (int i = 1; i < maxTime; i++) {
             if (nrace[i] > mrace[i]) cur_n_first = true;
             else if (nrace[i] < mrace[i]) cur_n_first = false;
             if (i != 1 && past_n_first != cur_n_first) {
                 change++;
             }
             past_n_first = cur_n_first;
-            if (nrace[i] == 0 || mrace[i] == 0) break;
         }
         System.out.print(change);
     }
