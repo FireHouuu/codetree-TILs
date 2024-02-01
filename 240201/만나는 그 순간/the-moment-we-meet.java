ce4c90edcb9b3dd4b64fcc2b967e178774aa86ce
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-02-01 10:14:33 +0900

    Title: 만나는 그 순간, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240201/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int n, m;
+    public static final int time = 1000;
+    public static int[] nline = new int[time+1];
+    public static int[] mline = new int[time+1];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        int tmp = 0;
+        nline[0] = 0;
+        mline[0] = 0;
+        char direction;
+
+        int ntime = 1;
+        for (int i = 1; i <= n; i++) {
+            direction = sc.next().charAt(0);
+            tmp = sc.nextInt();
+            int plus = 1;
+            if (direction == 'L') plus = -plus;
+            for (int j = ntime; j <= ntime + tmp; j++) 
+                nline[j] = nline[j-1] + plus; 
+            ntime = ntime + tmp;
+        }
+
+        int mtime = 1;
+        for (int i = 1; i <= m; i++) {
+            direction = sc.next().charAt(0);
+            tmp = sc.nextInt();
+            int plus = 1;
+            if (direction == 'L') plus = -plus;
+            for (int j = mtime; j <= mtime + tmp; j++) 
+                mline[j] = mline[j-1] + plus; 
+            mtime = mtime + tmp;
+        }
+
+        int meet = -1;
+        for (int i = 1; i <= 1000; i++) {
+            if (nline[i] == mline[i]) {
+                meet = i;
+                break;
+            }
+        }
+
+        System.out.print(meet);
+    }
+}
