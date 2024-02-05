331e4ba92d608fca99bef86c51b41c3e2679e1cb
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-02-05 16:29:35 +0900

    Title: 작은 구슬의 이동, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240205/작은 구슬의 이동/small-marble-movement.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int n,t;
+    public static int[] dx = new int[]{0, 1, -1, 0};
+    public static int[] dy = new int[]{1, 0, 0, -1};
+
+    public static boolean inRange(int x, int y, int n) {
+        return (1 <= x && 1 <= y && x <= n && y <= n);
+    }
+
+    public static int dir(char input) {
+        if (input == 'U') return 0;
+        else if (input == 'D') return 3;
+        else if (input == 'R') return 1;
+        else return 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt(); t = sc.nextInt();
+        int[][] plane = new int[n+1][n+1];
+        int x, y, ahead;
+        x = sc.nextInt(); y = sc.nextInt();
+        char d = sc.next().charAt(0);
+        ahead = dir(d);
+
+        while (t-- > 0) {
+
+            if (!inRange(x+dy[ahead], y+dx[ahead], n)) {
+                ahead = 3 - ahead;
+                continue;
+            }
+            x = x + dy[ahead];
+            y = y + dx[ahead];
+        }
+        System.out.print(x + " " + y);
+    }
+}
