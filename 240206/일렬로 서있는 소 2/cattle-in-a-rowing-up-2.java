e46246af19f50a0d1ff07a3cd89837cc2e9c2c8d
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-02-06 23:18:40 +0900

    Title: 일렬로 서있는 소 2, Time: 178ms, Memory: 10MB, Status: Passed - CodeTree

--- 240206/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
+++ 240206/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -13,7 +13,7 @@ public class Main {
         for(int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 for (int k = 0; k < n; k++) {
-                    if (i < j && j < k && list[i] < list[j] && list[j] < list[k]) count++;
+                    if (i < j && j < k && list[i] <= list[j] && list[j] <= list[k]) count++;
                 }
             }
         }
