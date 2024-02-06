97926c5b796fbefa0e0f1408067653483716c730
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-02-06 16:36:44 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 246ms, Memory: 12MB, Status: Passed - CodeTree

--- 240206/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.java
+++ 240206/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static int n, count, d, x, y;
     public static int[] dx = new int[]{0, -1, 0, 1};
-    public static int[] dy = new int[]{1, 0, -1, 0};
+    public static int[] dy = new int[]{-1, 0, 1, 0};
 
     public static boolean inRange(int x, int y) {
         return(0 <= x && x < n && 0 <= y && y < n);
@@ -11,10 +11,10 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        count = 1;
+        n = sc.nextInt(); 
+        count = n * n;
         int[][] plane = new int[n][n];
-        x = n / 2; y = n / 2; d = 0;
+        x = n - 1; y = n - 1; d = 0;
         int tx, ty;
         for (int i = 0; i < n * n; i++) {
             plane[x][y] = count;
@@ -24,7 +24,7 @@ public class Main {
                 tx = x + dx[d]; ty = y + dy[d];
             }
             x = tx; y = ty;
-            count++;
+            count--;
         }
 
         for (int i = 0; i < n; i++) {
