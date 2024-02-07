f71e72327c5ba1f13273f769abf2ac8ab2771f86
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-02-07 20:59:21 +0900

    Title: G or H 3, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240207/G or H 3/G-or-H-3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int n, k;
+    public static int[] line = new int[10000];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt(); k = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int location = sc.nextInt();
+            char alpha = sc.next().charAt(0);
+            if (alpha == 'G') line[location - 1] = 1;
+            else line[location - 1] = 2;
+        }
+        int max = 0;
+        for (int i = 0; i <= n - k; i++) {
+            int sum = 0;
+            for (int j = 0; j <= k; j++) 
+                sum = sum + line[i + j];
+            max = Math.max(max, sum);
+        }
+
+        System.out.print(max);
+    }
+}
