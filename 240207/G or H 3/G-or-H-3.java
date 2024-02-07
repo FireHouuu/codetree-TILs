4f2989160a8a7cc2c13ab36c741a6ffc761f69fe
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-02-07 21:27:32 +0900

    Title: G or H 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240207/G or H 3/G-or-H-3.java
+++ 240207/G or H 3/G-or-H-3.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static int n, k;
-    public static int[] line = new int[10000];
+    public static int[] line = new int[10001];
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt(); k = sc.nextInt();
         for (int i = 0; i < n; i++) {
             int location = sc.nextInt();
             char alpha = sc.next().charAt(0);
-            if (alpha == 'G') line[location - 1] = 1;
-            else line[location - 1] = 2;
+            if (alpha == 'G') line[location] = 1;
+            else line[location] = 2;
         }
         int max = 0;
-        for (int i = 0; i <= n - k; i++) {
+        for (int i = 0; i <= 10000; i++) {
             int sum = 0;
             for (int j = 0; j <= k; j++) 
                 sum = sum + line[i + j];
