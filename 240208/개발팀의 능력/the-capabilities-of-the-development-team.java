6c7dd3767e4e4a23ec9fe461bbb46e1c75a5f403
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-02-08 15:16:04 +0900

    Title: 개발팀의 능력, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240208/개발팀의 능력/the-capabilities-of-the-development-team.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] list = new int[5];
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            list[i] = sc.nextInt();
+            sum += list[i];
+        }
+        
+        int min_dif = -1;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = i+1; j < 5; j++) {
+                for (int k = 0; k < 4; k++) {
+                    for (int f = k+1; f < 5; f++) {
+                        if (k == i || f == j) continue;
+                        int first = list[i] + list[j];
+                        int second = list[k] + list[f];
+                        int third = sum - first - second;
+                        if (first != second && second != third && first != second) {
+                            min_dif = Integer.MAX_VALUE;
+                            int dif = Math.max(Math.abs(first-second), Math.abs(second-third));
+                            dif = Math.max(dif, Math.abs(first-third));
+                            min_dif = Math.min(min_dif, dif);
+                        }
+                    }
+                    
+                }
+            }
+        }
+        System.out.print(min_dif);
+    }
+}
