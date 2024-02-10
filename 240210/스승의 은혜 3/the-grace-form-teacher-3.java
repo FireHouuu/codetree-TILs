003141f0a0484c26493a05694509b58ed64f9e8f
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-02-10 11:41:33 +0900

    Title: 스승의 은혜 3, Time: 1227ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240210/스승의 은혜 3/the-grace-form-teacher-3.java
+++ 240210/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -13,7 +13,10 @@ public class Main {
         }
         Arrays.sort(ps, (o1, o2) -> Integer.compare(o1[0] + o1[1], o2[0] + o2[1]));
         int[][] ps_copy = new int[n][2];
-        ps_copy = ps;
+        for (int i = 0; i < n; i++) 
+            for (int j = 0; j < n; j++) 
+                ps_copy[j] = ps[j].clone(); 
+
         int max_count = 0;
         for (int i = 0; i < n; i++) {
             ps_copy[i][0] /= 2;
@@ -22,11 +25,17 @@ public class Main {
             int budget = b;
             for (int j = 0; j < n; j++) {
                 budget -= (ps_copy[j][0] + ps_copy[j][1]);
-                if (budget >= 0) count++;
+                if (budget >= 0) {
+                    count++;
+                    //System.out.print("last budget : " + budget + " / value : " + ps_copy[i][0] + " / count : " + count);
+                    //System.out.println();
+                }
                 else break;
             }
             max_count = Math.max(max_count, count);
-            ps_copy = ps;
+            for (int k = 0; k < n; k++) 
+                for (int f = 0; f < n; f++) 
+                    ps_copy[f] = ps[f].clone(); 
         }
         System.out.print(max_count);
     }
