b09da5753bd7228a111d67c888e27144619ecf07
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-02-10 12:01:28 +0900

    Title: 스승의 은혜 3, Time: 423ms, Memory: 19MB, Status: Passed - CodeTree

--- 240210/스승의 은혜 3/the-grace-form-teacher-3.java
+++ 240210/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -12,30 +12,20 @@ public class Main {
             ps[i][1] = sc.nextInt();
         }
         Arrays.sort(ps, (o1, o2) -> Integer.compare(o1[0] + o1[1], o2[0] + o2[1]));
-        int[][] ps_copy = new int[n][2];
-        for (int i = 0; i < n; i++) 
-            for (int j = 0; j < n; j++) 
-                ps_copy[j] = ps[j].clone(); 
 
         int max_count = 0;
         for (int i = 0; i < n; i++) {
-            ps_copy[i][0] /= 2;
-            Arrays.sort(ps_copy, (o1, o2) -> Integer.compare(o1[0] + o1[1], o2[0] + o2[1]));
-            int count = 0;
-            int budget = b;
+            int tmp = ps[i][0] / 2;
+            int count = 1;
+            int budget = b - tmp - ps[i][1];
             for (int j = 0; j < n; j++) {
-                budget -= (ps_copy[j][0] + ps_copy[j][1]);
-                if (budget >= 0) {
+                if (i == j || budget <= 0) continue;
+                budget -= (ps[j][0] + ps[j][1]);
+                if (budget >= 0) 
                     count++;
-                    //System.out.print("last budget : " + budget + " / value : " + ps_copy[i][0] + " / count : " + count);
-                    //System.out.println();
-                }
                 else break;
             }
             max_count = Math.max(max_count, count);
-            for (int k = 0; k < n; k++) 
-                for (int f = 0; f < n; f++) 
-                    ps_copy[f] = ps[f].clone(); 
         }
         System.out.print(max_count);
     }
