c3f66f62aa35c3f551becca5d415af9d09dfb287
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-02-13 20:05:20 +0900

    Title: 두 선분, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240213/두 선분/two-lines.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int a, b, c, d;
+    public static boolean is_intersect(int i, int j, int k) {
+        return (i > j || i < k);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt(); b = sc.nextInt(); c = sc.nextInt(); d = sc.nextInt();
+        boolean flag = true;
+        if (is_intersect(a, c, d)) flag = false;
+        else if (is_intersect(b, c, d)) flag = false;
+        else if (is_intersect(c, a, b)) flag = false;
+        else if (is_intersect(d, a, b)) flag = false;
+
+        if (flag) System.out.print("nonintersecting");
+        else System.out.print("intersecting");
+    }
+}
