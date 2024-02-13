3475a0f0019db0bec1fc365135d0b3d59dc62b31
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-02-13 16:52:55 +0900

    Title: 최대 H 점수 2, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240213/최대 H 점수 2/maximum-h-score-2.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int n, l;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt(); l = sc.nextInt();
+        int[] list = new int[n];
+        int max = 0;
+        int min = 100;
+        for (int i = 0; i < n; i++) {
+            list[i] = sc.nextInt();
+            max = Math.max(max, list[i]);
+            min = Math.min(min, list[i]);
+        }
+        int h_point = 0;
+        for (int i = max; i >= min; i--) {
+            if (i > n) {
+                continue;
+            }
+            int tmp_L = l;
+            boolean flag = true;
+            int tmp_point = 0;
+            for (int j = 0; j < n; j++) {
+                //System.out.print(list[j] + " " + i);
+                if (list[j] >= i - 1) {
+                    if (list[j] < i){
+                        if (tmp_L <= 0) {
+                            flag = false;
+                            break;
+                        }
+                        else {
+                            tmp_L--;
+                            tmp_point++;
+                        }
+                    }
+                    else {
+                        tmp_point++;
+                    }
+                }
+            }
+            if (!flag) continue;
+            if (tmp_point >= i) {
+                h_point = i;
+                
+                break;
+            }
+        }
+        System.out.print(h_point);
+    }
+}
