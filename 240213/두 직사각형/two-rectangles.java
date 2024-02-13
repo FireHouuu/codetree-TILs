e8ede310cbd1732e52660ee4531ecd16f1ab86bf
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-02-13 20:18:58 +0900

    Title: 두 직사각형, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240213/두 직사각형/two-rectangles.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int[] squre1 = new int[4];
+    public static int[] squre2 = new int[4];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 4; i++) squre1[i] = sc.nextInt();
+        for (int i = 0; i < 4; i++) squre2[i] = sc.nextInt();
+        boolean overlapping = true;
+        if ((squre1[2] < squre2[0]) || (squre2[2] < squre1[0]) || (squre1[3] < squre2[1]) || (squre2[3] < squre1[1]))
+            overlapping = false;
+        if (overlapping) System.out.print("overlapping");
+        else System.out.print("unoverlapping");
+    }
+}
