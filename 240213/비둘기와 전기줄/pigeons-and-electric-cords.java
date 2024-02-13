f5acf91aa0a61a167d49e19e31a83b55e3663903
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-02-13 21:30:50 +0900

    Title: 비둘기와 전기줄, Time: 163ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240213/비둘기와 전기줄/pigeons-and-electric-cords.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int[] list = new int[11];
+    public static int[] change = new int[11];
+    public static int n;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for (int i = 0; i < n; i++) list[i] = -1;
+        for (int i = 0; i < n; i++) {
+            int number = sc.nextInt();
+            int LR = sc.nextInt();
+            if (list[number] != LR) {
+                if (list[number] != -1) {
+                    //System.out.print(number + " / " + list[i] + " / " + LR);
+                    //System.out.println();
+                    change[number]++;
+                }
+                list[number] = LR;
+            } 
+        }
+        int cross = 0;
+        for (int i = 1; i <= 10; i++) 
+            cross += change[i];
+        System.out.print(cross);
+    }
+}
