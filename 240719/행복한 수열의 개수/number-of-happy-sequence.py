786a2b74aab9231b0bcf125418ca634f0b723ea0
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-07-19 14:59:40 +0900

    Title: 행복한 수열의 개수, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240719/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240719/행복한 수열의 개수/number-of-happy-sequence.py
@@ -6,29 +6,31 @@ grid = [
 ]
 
 output = 0
-for i in range(n):
-    x = 1 
-    y = 1
-    for j in range(n-1):
-        if x == m:
-            output += 1
-            x = -1
-        if x != -1: 
-            if grid[i][j] == grid[i][j+1]:
-                x += 1
-            else:
-                x = 1
-        
-        if y == m:
-            output += 1
-            y = -1
-        if y != -1:
-            if grid[j][i] == grid[j+1][i]:
-                y += 1
-                if y == m:
-                    output += 1
-            else:
-                y = 1
 
-if n == 1: output = 2
+for i in range(n):
+    x_count = 1
+    y_count = 1
+    for j in range(n-1):
+        # 가로 방향 검사
+        if grid[i][j] == grid[i][j+1]:
+            x_count += 1
+        else:
+            x_count = 1
+        if x_count == m:
+            output += 1
+            x_count = 0  # 중복 카운트를 막기 위해 0으로 설정
+        
+        # 세로 방향 검사
+        if grid[j][i] == grid[j+1][i]:
+            y_count += 1
+        else:
+            y_count = 1
+        if y_count == m:
+            output += 1
+            y_count = 0  # 중복 카운트를 막기 위해 0으로 설정
+
+# 특수 경우: n이 1일 때, 모든 값이 같은 것으로 처리
+if n == 1:
+    output = 2 if grid[0][0] == m else 0
+
 print(output)
