1d6a49f8864992599b3caafa9456198fb01b6cac
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-07-19 14:28:19 +0900

    Title: 행복한 수열의 개수, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240719/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240719/행복한 수열의 개수/number-of-happy-sequence.py
@@ -10,18 +10,22 @@ for i in range(n):
     x = 1 
     y = 1
     for j in range(n-1):
-        if grid[i][j] == grid[i][j+1]:
-            x += 1
-            if x == m:
-                output += 1
-        else:
-            x = 1
+        if x == m:
+            output += 1
+        else: 
+            if grid[i][j] == grid[i][j+1]:
+                x += 1
+            else:
+                x = 1
         
-        if grid[j][i] == grid[j+1][i]:
-            y += 1
-            if y == m:
-                output += 1
+        if y == m:
+            output += 1
         else:
-            y = 1
+            if grid[j][i] == grid[j+1][i]:
+                y += 1
+                if y == m:
+                    output += 1
+            else:
+                y = 1
 
 print(output)
