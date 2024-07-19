43cc4ede26bbdcac346adfb04617435749b4ee38
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-07-19 14:14:34 +0900

    Title: 행복한 수열의 개수, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240719/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,27 @@
+n, m = map(int, input().split())
+
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+output = 0
+for i in range(n):
+    x = 1 
+    y = 1
+    for j in range(n-1):
+        if grid[i][j] == grid[i][j+1]:
+            x += 1
+            if x == m:
+                output += 1
+        else:
+            x = 1
+        
+        if grid[j][i] == grid[j+1][i]:
+            y += 1
+            if y == m:
+                output += 1
+        else:
+            y = 1
+
+print(output)
