4cc7d9f1c426de76fbb420e85f2f0fec41832aac
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-07-24 17:09:15 +0900

    Title: 컨베이어 벨트, Time: 111ms, Memory: 25MB, Status: Passed - CodeTree

--- 240724/컨베이어 벨트/conveyor-belt.py
+++ 240724/컨베이어 벨트/conveyor-belt.py
@@ -4,12 +4,12 @@ bot = list(map(int, input().split()))
 
 for _ in range(t):
     top_temp = top[n-1]
-    bot_temp = bot[0]
+    bot_temp = bot[n-1]
     for i in range(n-1):
         top[n-i-1] = top[n-i-2]
-        bot[i] = bot[i+1]
+        bot[n-i-1] = bot[n-i-2]
     top[0] = bot_temp
-    bot[n-1] = top_temp
+    bot[0] = top_temp
 
 for i in range(n):
     print(top[i], end=' ')
