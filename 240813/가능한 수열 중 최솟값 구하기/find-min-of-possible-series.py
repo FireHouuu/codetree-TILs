e665fe2bb8c2fa5f0025ba7a4182415463aa50ce
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-08-13 14:56:35 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 240813/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
+++ 240813/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
@@ -1,34 +1,28 @@
+import sys
+
 n = int(input())
 ans = []
-flag = True
 
 def print_list():
-    for i in range(n):
-        print(ans[i], end = "")
-    print()
+    print("".join(map(str, ans)))
 
 def check():
     global n
-    k = int(n/2)
-    for i in range(1, k+1): #길이
-        for j in range(n-1): #위치
-            if j + i * 2 > n:
-                break
-            if ans[j:j+i] == ans[j+i:j+i*2]:
-                return False
+    k = int(len(ans) / 2)
+    for i in range(1, k + 1):  # 길이
+        if ans[-i:] == ans[-2 * i:-i]:  # 패턴 비교 (최신 부분만 확인)
+            return False
     return True
 
 def find(curr_num):
-    global flag
     if curr_num == n + 1:
-        if check():
-            print_list()
-            flag = False
+        print_list()
+        sys.exit(0)  # 첫 번째 유효한 수열을 찾으면 프로그램 종료
         return
     
     for i in range(4, 7):
         ans.append(i)
-        if flag:
+        if check():  # 패턴이 유효한지 미리 확인
             find(curr_num + 1)
         ans.pop()
 
