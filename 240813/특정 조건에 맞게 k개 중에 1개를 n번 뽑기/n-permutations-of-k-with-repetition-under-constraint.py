680d3ca28419a209fa46075d90cbd69d12182f3c
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-08-13 13:16:04 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 327ms, Memory: 29MB, Status: Passed - CodeTree

--- 240813/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
+++ 240813/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
@@ -10,7 +10,7 @@ def choose(curr_num):
         return
     
     for i in range(1, k+1):
-        if curr_num == 1 or curr_num == 2 or (ans[-1] != i and ans[-2] != i):
+        if curr_num == 1 or curr_num == 2 or (ans[-1] != i or ans[-2] != i):
             ans.append(i)
             choose(curr_num + 1)
             ans.pop()
