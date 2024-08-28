b0997852d72c8e6b82233c909f0a1c7b3fdc9bba
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-08-28 20:52:02 +0900

    Title: 초대장과 번호표, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240828/초대장과 번호표/invitation-and-number-tag.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Iterator;
+
+public class Main {
+    public static int n, g;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt(); g = sc.nextInt();
+        HashSet<Integer>[] set = new HashSet[g];
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < g; i++) {
+            int num = sc.nextInt();
+            set[i] = new HashSet<Integer>();
+            for (int j = 0; j < num; j++) 
+                set[i].add(j);
+        }
+        queue.add(1);
+        int ans = 1;
+        while(!queue.isEmpty()) {
+            int check = queue.poll();
+            for (int i = 0; i < g; i++) {
+                if (set[i].contains(check)) {
+                    set[i].remove(check);
+                    if (set[i].size() == 1) {
+                        Iterator<Integer> iterator = set[i].iterator();
+                        int element = 0;
+                        if (iterator.hasNext()) {
+                            element = iterator.next();
+                        }
+                        queue.add(element);
+                        set[i].remove(element);
+                        ans++;
+                    }
+                } 
+            }
+        }
+        
+        System.out.println(ans);
+    }
+}
