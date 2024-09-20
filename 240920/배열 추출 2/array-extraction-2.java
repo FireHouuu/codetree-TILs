4849849b642ea9a4f613c63dc92924e532343bad
FireHouuu <62393776+FireHouuu@users.noreply.github.com>
2024-09-20 14:47:20 +0900

    Title: 배열 추출 2, Time: 1321ms, Memory: 34MB, Status: Passed - CodeTree

--- 240920/배열 추출 2/array-extraction-2.java
+++ 240920/배열 추출 2/array-extraction-2.java
@@ -2,40 +2,50 @@ import java.util.Scanner;
 import java.util.PriorityQueue;
 
 class Pair implements Comparable<Pair> {
-    int normal, abs;
+    int absX, x;
 
-    public Pair(int normal) {
-        this.normal = normal;
-        this.abs = Math.abs(normal);
+    public Pair(int absX, int x) {
+        this.absX = absX;
+        this.x = x;
     }
 
     @Override
     public int compareTo(Pair p) {
-        if (this.abs == p.abs) {
-            return Integer.compare(this.normal, p.normal);
-        } else {
-            return Integer.compare(this.abs, p.abs);
-        }
+        if(this.absX != p.absX)
+            return this.absX - p.absX;  // |x| 기준 오름차순 정렬
+        else
+            return this.x - p.x;        // |x|가 같다면, x 기준 오름차순 정렬
     }
 }
+
 public class Main {
+    // 변수 선언:
     public static int n;
-    public static PriorityQueue<Pair> q = new PriorityQueue<>();
+    
+    public static PriorityQueue<Pair> pq = new PriorityQueue<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // 입력:
         n = sc.nextInt();
 
-        for (int i = 0; i < n; i++) {
-            int tmp = sc.nextInt();
-            if (tmp == 0) {
-                if (q.isEmpty()) {
+        for(int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+
+            // x가 0이 아니라면
+            // 우선순위 큐에 넣어줍니다. 
+            // (|x|, x) 형태로 넣어줍니다.
+            if(x != 0)
+                pq.add(new Pair(Math.abs(x), x));
+            // x가 0이라면
+            // 가장 앞에 있는 원소를 출력해주고 제거합니다.
+            else {
+                // 우선순위 큐가 비어져 있다면 0을 출력하고 넘어갑니다.
+                if(pq.isEmpty()) {
                     System.out.println(0);
-                } else {
-                    System.out.println(q.poll().normal);
+                    continue;
                 }
-            } else {
-                Pair tmp_p = new Pair(tmp);
-                q.add(tmp_p);
+                System.out.println(pq.poll().x);
             }
         }
     }
